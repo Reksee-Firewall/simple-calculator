ff27465451d779697b348f800aad9bd7ee42ae78
Reksee <ramonfernando@id.uff.br>
2023-08-19 20:30:06 -0300

    Release: Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # simple-calculator
 A calculator with the basic math operators. 
+
+# The Odin Project Command 
+
+Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+
+- add; 
+- subtract;
+- divide;
+- multiply.
+
+A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+
+Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key. Add also a "clear" button.
+
+Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
+
+Users should be able to string together several operations and get the right answer, with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42.
+
+Your calculator should not evaluate more than a single pair of numbers at a time. 
+
+You should round answers with long decimals so that they don’t overflow the screen.
+
+Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals!
+
+Add a “backspace” button, so the user can undo if they click the wrong number.
+
+Add keyboard support!
---
+++ main.js
@@ -0,0 +1,386 @@
+// Create Numeric Keyboard
+const keyboard = document.querySelector("#numeric-keyboard");
+const clearArray = ["AC", "C", "Backspace", "Escape"];
+const operatorArray = ["%", "÷", "+", "-", "×",
+                        "/", "*", "Enter"];
+const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const charArray = ["AC", "C", "%", "÷", 7, 8, 9, "+", 4, 5, 6, "-",
+                    1, 2, 3, "×", 0, "."];
+for (let i = 0; i < 5; i++) {
+    const line = document.createElement("div");
+    line.setAttribute("id", `lineNumber${i}`);
+    line.classList.add("lines");
+    for (let j = 0; j < 4; j++) {
+        if (((j + 4 * i) !== 18) && ((j + 4 * i) !== 19)) {
+            const key = document.createElement("div");
+            key.setAttribute("id", `keyNumber${j + 4 * i}`);
+            key.classList.add("keys");
+            key.textContent = charArray[j + 4 * i];
+            line.appendChild(key);
+        } else {
+            if ((j + 4 * i) === 18) {
+                const key = document.createElement("div");
+                key.setAttribute("id", `keyNumber${j + 4 * i}`);
+                key.classList.add("keys");
+                key.textContent = '=';
+                line.appendChild(key);
+            }
+        }
+    }
+    keyboard.appendChild(line);
+}
+
+// Yeah, this code was made in a rush. It's quite a mess. 
+
+let validEquation = [];
+
+function truncateNumbers() {
+    let firstNumber = "", secondNumber = "", i = 0;
+    let operatorIndex = 0;
+    do {
+        firstNumber += `${(validEquation[i])}`;
+        i++;
+        operatorIndex = i;
+    } while(typeof(validEquation[i]) !== "string");
+    for (let j = operatorIndex + 1; j < validEquation.length; j++) {
+        secondNumber += `${(validEquation[j])}`;
+    }
+    validEquation = [Number(firstNumber), validEquation[operatorIndex], Number(secondNumber)];
+}
+
+function evaluateEquation(n1, n2, operator) {
+    let answer;
+    let stringNumber = '';
+    switch(operator) {
+        case "%":
+            answer = ((n1 / 100) * n2);
+            break;
+        case "÷":
+            answer = (n1 / n2);
+            break;
+        case "+":
+            answer = (n1 + n2);
+            break;
+        case "-":
+            answer = (n1 - n2);
+            break;
+        case "×": 
+            answer = (n1 * n2);
+            break;
+        default:
+            console.error("Invalid operation."); 
+            break;
+    }
+    stringNumber = `${(answer)}`;
+    if (stringNumber.split('').length > 7) {
+        stringNumber = (stringNumber.slice(7));
+    }
+    return parseFloat(stringNumber);
+}
+
+const whereValuesAreShown = document.querySelector("#showValues");
+
+// validEquation has always the same format: [number, operator, number].
+function updateEquationResult() {
+    truncateNumbers();
+    console.log(validEquation);
+    const equationSize = validEquation.length;
+    if (equationSize === 1) {
+        return validEquation[0];
+    } else if (equationSize === 2 ) {
+        return validEquation[0]; 
+    } else {
+        const n1 = validEquation[0];
+        const n2 = validEquation[2]; 
+        const operator = validEquation[1];
+        const result = evaluateEquation(n1, n2, operator);
+        // Remove 3 elements starting from index 0.
+        validEquation.splice(0, 3);
+        validEquation.push(result);
+        whereValuesAreShown.textContent = result;
+        return validEquation[0];
+    }
+}
+
+function floatUpdate() {
+    const n1 = validEquation[0];
+    const n2 = validEquation[2]; 
+    const operator = validEquation[1];
+    const result = evaluateEquation(n1, n2, operator);
+    // Remove 3 elements starting from index 0.
+    validEquation.splice(0, 3);
+    validEquation.push(result);
+    return validEquation[0];
+}
+
+// Hovering Keys
+const keys = keyboard.querySelectorAll(".keys");
+const keyArray = Array.from(keys);
+let operKey;
+let hitCounter = 0;
+let nextNumberFloat = false;
+
+keyArray.forEach((key) => {
+    key.addEventListener('mouseenter', () => {
+        key.style.marginBottom = '0.5rem';
+        key.style.color = "hsl(0, 0%, 0%)";
+        if (clearArray.some((char) => (char === key.textContent))) {
+            key.classList.add("turnRed");
+        } else if (operatorArray.some((char) => (char === key.textContent))) {
+            key.classList.add("turnBlue");
+        } else if (numbersArray.some((char) => (char === key.textContent))) {
+            key.classList.add("turnWhite");
+        } else {
+            key.classList.add("turnGreen");
+        }
+    });
+    key.addEventListener('mouseleave', () => {
+        key.style.marginBottom = '0rem';
+        if (!key.classList.contains("operatorSelected")) {
+            key.style.color = "hsl(0, 0%, 100%)";
+        }
+        if (clearArray.some((char) => (char === key.textContent))) {
+            key.classList.remove("turnRed");
+        } else if (operatorArray.some((char) => (char === key.textContent))) {
+            key.classList.remove("turnBlue");
+        } else if (numbersArray.some((char) => (char === key.textContent))) {
+            key.classList.remove("turnWhite");
+        } else {
+            key.classList.remove("turnGreen");
+        }
+    });
+    key.addEventListener('click', () => {
+        key.style.marginBottom = '0rem';
+        setTimeout(() => {key.style.marginBottom = '0.5rem'}, 100);
+
+        if (key.textContent === "=") {
+            if (validEquation.length === 0 || validEquation.length === 1 || !validEquation.some((str) => typeof(str) === "string")) {
+                console.error("There is not enough values within the array.");
+                return null;
+            }
+        }
+
+        if (clearArray.some((char) => (char === key.textContent))) {
+            switch(key.textContent) {
+                case "AC":
+                    validEquation = [];
+                    whereValuesAreShown.textContent = "WIPED";
+                    let floatPoint = document.querySelector("#keyNumber17");
+                    floatPoint.classList.remove("operatorSelected");
+                    floatPoint.style.color = "hsl(0, 0%, 100%)";
+                    for (operKey of keys) {
+                        if (operatorArray.includes(operKey.textContent) ) {
+                            operKey.style.color = 'hsl(0, 0%, 100%)';
+                            operKey.classList.remove("operatorSelected");
+                            hitCounter = validEquation.length - 1;
+                        }
+                    }
+                    break;
+                case "C": 
+                    if (typeof(validEquation[validEquation.length - 1]) !== "string") {
+                        whereValuesAreShown.textContent = whereValuesAreShown.textContent.slice(0, -1);
+                        validEquation.pop();
+                        hitCounter--;
+                    } else {
+                        for (operKey of keys) {
+                            if (operatorArray.includes(operKey.textContent) ) {
+                                operKey.style.color = 'hsl(0, 0%, 100%)';
+                                operKey.classList.remove("operatorSelected");
+                                hitCounter = validEquation.length - 1;
+                            }
+                        }
+                    }
+                    break; 
+                default: 
+                    console.error("Error!");
+            }
+        } else if (operatorArray.some((char) => (char === key.textContent))) {
+            if (validEquation.length === 0) {
+                validEquation.push(Number(0));
+            } 
+            if (validEquation.some((elem) => (typeof(elem) === "string"))) {
+                if (typeof(validEquation[validEquation.length - 1]) === "string") {
+                    console.error("ERROR: TWO OPERATORS ENTRY");
+                } else {
+                    console.log(updateEquationResult());
+                }
+            }
+            key.classList.add("operatorSelected");
+            validEquation.push(key.textContent);
+            hitCounter = 0;
+        } else if (numbersArray.some((char) => (char === key.textContent))) {
+            if (validEquation.some((elem) => (typeof(elem) === "string"))) {
+                // We use 'of' so that the loop may affect directly the DOOM elements within 'keys'.
+                for (operKey of keys) {
+                    if (operatorArray.includes(operKey.textContent)) {
+                        operKey.style.color = 'hsl(0, 0%, 100%)';
+                        operKey.classList.remove("operatorSelected");
+                    }
+                }
+            } 
+            whereValuesAreShown.textContent = '';
+            if (validEquation.some((elem) => (typeof(elem) === "string"))) {
+                i = 0;
+                do {
+                    i++;
+                    operatorIndex = i;
+                } while(typeof(validEquation[i]) !== "string");
+                if (hitCounter < 7) {
+                    hitCounter++;
+                    if (key.textContent !== '.') {
+                        let floatPoint = document.querySelector("#keyNumber17");
+                        floatPoint.classList.remove("operatorSelected");
+                        floatPoint.style.color = "hsl(0, 0%, 100%)";
+                        if (nextNumberFloat) {
+                            let integerPart = validEquation[validEquation.length - 1];
+                            validEquation[validEquation.length - 1] = (parseFloat(integerPart + Number(key.textContent) / 10));
+                            nextNumberFloat = false;
+                        } else {
+                            validEquation.push(Number(key.textContent));
+                        }
+                    } else {
+                        if (validEquation.splice(operatorIndex + 1).length !== 0) {
+                            key.classList.add("operatorSelected");
+                            nextNumberFloat = true;
+                        }
+                    }
+                }
+                for (i = operatorIndex + 1; i < validEquation.length; i++) {
+                    whereValuesAreShown.textContent += `${validEquation[i]}`;
+                } 
+            } else {
+                if (hitCounter < 7) {
+                    hitCounter++;
+                    if (key.textContent !== '.') {
+                        let floatPoint = document.querySelector("#keyNumber17");
+                        floatPoint.classList.remove("operatorSelected");
+                        floatPoint.style.color = "hsl(0, 0%, 100%)";
+                        if (nextNumberFloat) {
+                            validEquation.push("+", parseFloat(Number(key.textContent) / 10));
+                            floatUpdate();
+                            nextNumberFloat = false;
+                        } else {
+                            validEquation.push(Number(key.textContent));
+                        }
+                    } else {
+                        if (validEquation.length !== 0) {
+                            key.classList.add("operatorSelected");
+                            nextNumberFloat = true;
+                        }
+                    }
+                }
+                for (i = 0; i < validEquation.length; i++) {
+                    whereValuesAreShown.textContent += `${validEquation[i]}`;
+                } 
+            }
+        } else {
+            console.log(updateEquationResult());
+        }
+    });
+});
+
+// Keyboard Support
+let setKey;
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    switch(e.key) {
+        case '0':
+            setKey = document.querySelector("#keyNumber16");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '1':
+            setKey = document.querySelector("#keyNumber12");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '2':
+            setKey = document.querySelector("#keyNumber13");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '3':
+            setKey = document.querySelector("#keyNumber14");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '4':
+            setKey = document.querySelector("#keyNumber8");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '5': 
+            setKey = document.querySelector("#keyNumber9");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '6':
+            setKey = document.querySelector("#keyNumber10");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '7':
+            setKey = document.querySelector("#keyNumber4");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case '8':
+            setKey = document.querySelector("#keyNumber5");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break; 
+        case '9':
+            setKey = document.querySelector("#keyNumber6");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break; 
+        case "Backspace":
+            setKey = document.querySelector("#keyNumber1");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "Escape": 
+            setKey = document.querySelector("#keyNumber0");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "/":
+            setKey = document.querySelector("#keyNumber3");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "*":
+            setKey = document.querySelector("#keyNumber15");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "-":
+            setKey = document.querySelector("#keyNumber11");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "+":
+            setKey = document.querySelector("#keyNumber7");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        case "Enter":
+            setKey = document.querySelector("#keyNumber18");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break; 
+        case ".":
+            setKey = document.querySelector("#keyNumber17");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break; 
+        case "%":
+            setKey = document.querySelector("#keyNumber2");
+            setKey.click();
+            setTimeout(() => setKey.style.marginBottom = '0rem', 200);
+            break;
+        default:
+            console.log("Unavailable key.");
+            break;
+    }
+})
